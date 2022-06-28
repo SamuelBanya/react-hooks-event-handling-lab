7d45fcbf56a9d1f3e5f0cde5b2cd407fd29fdd2c
Samuel Banya <sbanya@fastmail.com>
2022-06-28 19:08:48 -0400

    Adding latest changes to assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+  console.log("Keypad test");
+
+  function enterPasswordEvent() {
+    console.log("Entering password...");
+  }
+
+  return <input onChange={() => enterPasswordEvent()} type="password" />;
+}
+
+export default Keypad;
