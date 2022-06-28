b50ca5427f16a24ded8843604689757e5e4dd1b5
Samuel Banya <sbanya@fastmail.com>
2022-06-28 19:08:39 -0400

    commit tree a39172022fef0bee60b7ef25d084c0d2f4cac903

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  console.log("Test");
+
+  function focusEventHandler() {
+    console.log("Good!");
+  }
+
+  function blurEventHandler() {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button onFocus={focusEventHandler} onBlur={blurEventHandler}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
